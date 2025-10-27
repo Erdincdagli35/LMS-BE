949675b903acb20521dcc489c5fd613a529666f7
erdindagli35 <36699257+Erdincdagli35@users.noreply.github.com>
2025-10-27 10:32:20 +0300

    Update CORS configuration for BookController

--- src/main/java/com/edsoft/lms/controller/BookController.java
+++ src/main/java/com/edsoft/lms/controller/BookController.java
@@ -24,7 +24,7 @@ import java.util.List;
 @RequestMapping("/books")
 @EnableAutoConfiguration
 @CrossOrigin(origins = {"http://localhost:4200",
-                        "https://librarymanagementsystem.up.railway.app"})
+                        "https://lms.up.railway.app"})
 public class BookController {
 
     @Autowired
